02af3604888ec9be3d1dab25190a93344e60917c
oscar <oscar.carrillo758@gmail.com>
2025-01-05 12:09:45 -0800

    update commit

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
 /** Database setup for BizTime. */
 const { Client } = require("pg");
 
+let DB_URI;
+
+if(process.env.NODE_ENV === "test"){
+  DB_URI = "postgresql:///biztime_test";
+} else {
+  DB_URI = "postgresql:///biztime";
+}
+
 const client = new Client({
-  connectionString: "postgresql:///biztime"
+   connectionString: DB_URI
 });
 
-client.connect();
+let db = new Client({
+  host : "/var/run/postgresql",
+  database : DB_URI
+})
 
-module.exports = client;
+db.connect();
+
+module.exports = db;
 
